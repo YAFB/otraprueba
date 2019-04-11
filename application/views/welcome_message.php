5db1bc623b0a8f071ea9dd9a1e140d58893df595
Frida <fridamzt26@gmail.com>
2019-04-11 11:40:22 -0600

    Hola

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -71,9 +71,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Ey que onda</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p>Vamos por una maruchan?</p>
 		<code>application/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
