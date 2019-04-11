8f8d07264c2b08366031e2801d2fd203fdc63a0d
YAFB <49499670+YAFB@users.noreply.github.com>
2019-04-11 11:38:22 -0600

    cambio de titulos

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,12 +68,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>hola a todos</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p>Dale click</p>
 		<code>application/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
